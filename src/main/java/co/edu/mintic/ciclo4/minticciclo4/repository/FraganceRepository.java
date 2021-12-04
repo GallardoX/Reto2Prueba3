c429884cf382e9470dc914e902dbc1085f912361
JuanGallardo <juan.gallardo.mt@correo.usa.edu.co>
2021-12-04 00:19:45 -0500

    commit1

---
+++ src/main/java/co/edu/mintic/ciclo4/minticciclo4/repository/FraganceRepository.java
@@ -0,0 +1,7 @@
+package co.edu.mintic.ciclo4.minticciclo4.repository;
+
+import co.edu.mintic.ciclo4.minticciclo4.model.Fragance;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FraganceRepository extends MongoRepository<Fragance, String> {
+}
